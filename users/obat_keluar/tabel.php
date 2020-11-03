667937eec536c29920633d2f2428ccf20b1928de
Angga-ops <rizkyangga16140@gmail.com>
2020-11-03 15:13:24 +0700

    ela gudang

---
+++ users/obat_keluar/tabel.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SESSION['level']!='admin' && $_SESSION['level']!='apotik' && $_SESSION['level']!='gudang'){ echo "<script>window.location='users?menu=404';</script>";}
+
+$cek_data = mysqli_query($con, "SELECT * FROM tbl_obat_keluar ORDER BY id_keluar DESC");
+?>
+<table id="data_tables" class="table table-bordered table-striped table-hover" width="100%">
+  <thead>
+    <tr>
+      <th width="1%">No.</th>
+      <th width="19%">Tanggal Keluar</th>
+      <th width="25%">Nama Obat</th>
+      <th width="55%">Jumlah Keluar</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    $no=1;
+    while ($baris = mysqli_fetch_array($cek_data)) {?>
+      <tr>
+        <td><?php echo $no++; ?></td>
+        <td><?php echo date('d-m-Y',strtotime($baris['tanggal_keluar'])); ?></td>
+        <td><?php echo $baris['jumlah_keluar']; ?></td>
+      </tr>
+    <?php
+    } ?>
+  </tbody>
+</table>
